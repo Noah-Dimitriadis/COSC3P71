8fd32e7518135aa3c0dfcb0d8933ca693e376434
Noah-Dimitriadis <noahdimitriadis2004@gmail.com>
2024-10-19 13:20:15 -0400

    Comments for submission

--- A1/Cell.py
+++ A1/Cell.py
@@ -1,11 +1,20 @@
+'''
+Noah Dimitriadis
+Student number: 7558224
+Email: nd22zr@brocku.ca
+
+This is a very basic cell class I made so I could easily grab the row and column numbers of a value
+'''
 class Cell:
     def __init__(self, value:str, row:int, col:int) -> None:
         self.value = value
         self.row = row
         self.col = col
 
-    def __str__(self):
+    def __str__(self):      # to make printing when debugging easier
         return f'Value: {self.value}|Row:  {self.row}|Col: {self.col}'
+
+    # The following functions I had to implement for the PriorityQueue class as it had no way of comparing the gameboard lists otherwise
 
     def __lt__(self, other):
         return self.value < other.value
